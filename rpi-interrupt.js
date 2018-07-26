0f193bc6ca2e18bfd9cdebd84196b422185fbe2e
FF077 <miftahf77@gmail.com>
2018-07-26 15:41:20 +0700

    Interrupt for Raspi 3

--- rpi-interrupt.js
+++ rpi-interrupt.js
@@ -4,13 +4,14 @@ var sensor_pin=25
 var interrupt_pin = new Gpio(sensor_pin, 'in', 'both'); 
  
 console.log('Ready...');
+var val=0
  
 // The callback passed to watch will be called when the button on GPIO #18 is
 // pressed. 
 interrupt_pin.watch(function (err, value) {
     if (err) throw err;
- 
-    console.log('Data ' + value);
+    val=val+1;
+    console.log('Data ' + val);
  
     // interrupt_pin.unexport(); // Unexport GPIO and free resources
 });
