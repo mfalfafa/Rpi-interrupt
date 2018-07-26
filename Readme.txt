6f36ac689ac2b1c4b845e0c4b67acb80e557cc7a
FF077 <miftahf77@gmail.com>
2018-07-26 15:48:53 +0700

    Interrupt for Raspi 3

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+## miftahf77@gmail.com
+## 26 July 2018
+
+This is a project to detect goods in pin 25 in raspi 3 using interrupt.
+You can use either Python script or Javascript to detect goods in Rpi 3 pin.
--- rpi-interrupt.js
+++ rpi-interrupt.js
@@ -1,17 +1,17 @@
 var Gpio = require('onoff').Gpio
 const sensor_pin=25
-// Export GPIO #18 as an interrupt, generating input.
+// Export GPIO #18 as an interrupt, generating input. Debounce timeout is 10 ms.
 const interrupt_pin = new Gpio(sensor_pin, 'in', 'rising', {debounceTimeout: 10}); 
  
 console.log('Ready...');
 var val=0
  
-// The callback passed to watch will be called when the button on GPIO #18 is
-// pressed. 
+// The callback passed to watch will be called when the interrupt_pin on GPIO #25 is triggered by goods. 
 interrupt_pin.watch(function (err, value) {
     if (err) throw err;
     val=val+1;
     console.log('Data ' + val);
  
+    // To stop watch to the sensor pin
     // interrupt_pin.unexport(); // Unexport GPIO and free resources
 });
